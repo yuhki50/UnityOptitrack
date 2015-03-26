bc5acc6c9c0bca48c683e1656b47c2babd25b9c6
yuhki50 <yuhki50@gmail.com>
2015-03-26 21:10:43 +0900

    OptiTrackのRigidBodyの位置を個別に調整できるように修正

--- Scripts/OptiTrackObject.cs
+++ Scripts/OptiTrackObject.cs
@@ -8,6 +8,7 @@ using System.Collections;
 public class OptiTrackObject : MonoBehaviour
 {
 	public int rigidbodyIndex;
+	public Vector3 positionOffset;
 	public Vector3 rotationOffset;
 	
 	void Start ()
@@ -18,9 +19,11 @@ public class OptiTrackObject : MonoBehaviour
 	void Update ()
 	{
 		Vector3 pos = OptiTrackManager.Instance.getPosition (rigidbodyIndex);
-		Quaternion rot = OptiTrackManager.Instance.getOrientation (rigidbodyIndex);
-		rot = rot * Quaternion.Euler (rotationOffset);
+		pos += positionOffset;
 		this.transform.position = pos;
+
+		Quaternion rot = OptiTrackManager.Instance.getOrientation (rigidbodyIndex);
+		rot *= Quaternion.Euler (rotationOffset);
 		this.transform.rotation = rot;
 	}
 }
