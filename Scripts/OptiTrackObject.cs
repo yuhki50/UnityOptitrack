d016b490bafbe40f189a1c3327b152d795d9be92
yuhki50 <yuhki50@gmail.com>
2015-04-08 16:15:58 +0900

    ローカル座標で調整できるように修正

--- Scripts/OptiTrackObject.cs
+++ Scripts/OptiTrackObject.cs
@@ -7,8 +7,10 @@ using UnityEngine;
 public class OptiTrackObject : MonoBehaviour
 {
 	public int RigidbodyIndex;
-	public Vector3 PositionOffset;	
+	public Vector3 PositionOffset;
 	public Vector3 RotationOffset;
+	public Vector3 LocalPositionOffset;
+	public Vector3 LocalRotationOffset;
 
 	void Start ()
 	{
@@ -18,11 +20,20 @@ public class OptiTrackObject : MonoBehaviour
 	void Update ()
 	{
 		var position = OptiTrackManager.Instance.getPosition (RigidbodyIndex);
+		var rotation = OptiTrackManager.Instance.getOrientation (RigidbodyIndex);
+
+		var isTracked = position == OptiTrackManager.Instance.origin;  //FIXME
+		if (isTracked) {
+			return;
+		}
+
+		rotation *= Quaternion.Euler (RotationOffset);
+		this.transform.rotation = rotation;
+
 		position += PositionOffset;
 		this.transform.position = position;
 
-		var rotation = OptiTrackManager.Instance.getOrientation (RigidbodyIndex);
-		rotation *= Quaternion.Euler (RotationOffset);
-		this.transform.rotation = rotation;
+		this.transform.Translate(LocalPositionOffset);
+		this.transform.Rotate(LocalRotationOffset);
 	}
 }
